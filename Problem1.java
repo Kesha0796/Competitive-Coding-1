408a5d5d4f685990471b06cbde2e19b209d5957c
Kesha0796 <kesharshah83@gmail.com>
2019-12-31 19:32:09 -0800

    Missing Number

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,20 @@
 
+// Find missing number
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[0]==1)
+                return 0;
+            else
+            {
+                if(nums[i]-nums[i-1]!=1)
+                    return nums[i]-1;
+            }
+        }
+        if(nums.length==1 && nums[0]==1)
+            return 0;
+        return nums[nums.length-1]+1;
+    }
+}
